32a29bfafec0f626957d43a42ff11254859dc5ad
gitfname <newamailnam@gmail.com>
2023-06-15 18:33:54 +0430

    stats section created

---
+++ src/components/AnimatedStatCount.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useRef, useState  } from "react"
+
+interface props {
+    start?: number,
+    end?: number,
+    speed?: number,
+    className?: string,
+    counterClassName?: string
+}
+
+function AnimatedStatCount({ start=0, end=100, speed=100, className="", counterClassName="" }: props) {
+    const [count, setCount] = useState(start)
+    const containerRef:any = useRef()
+    const [intervalId, setIntervalId] = useState()
+
+    if(count >= end) clearInterval(intervalId)
+
+    useEffect(
+        () => {
+            let id: any
+            const observer = new IntersectionObserver((entries) => {
+
+                if(entries[0].isIntersecting) {
+                    id = setInterval(() => {
+                        setCount(count => count + 1)
+                    }, speed);
+                    setIntervalId(id)
+                }
+
+            })
+
+            observer.observe(containerRef.current)
+
+            return () => {
+                observer.disconnect()
+                clearInterval(id)
+            }
+        },
+        []
+    )
+
+    return (
+        <div
+            ref={containerRef}
+            className={
+                ""
+                + " " + className
+            }
+        >
+            <p
+                className={
+                    "text-xl text-white font-bold"
+                    + " " + counterClassName 
+                }
+            >
+                {count}
+            </p>
+        </div>
+    )
+}
+
+export default AnimatedStatCount
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import AboutUs_1 from "../components/AboutUs_1"
 import Hero from "../components/Hero"
 import Services_1 from "../components/Services_1"
+import AnimatedStatCount from "../components/AnimatedStatCount"
 
 function Home() {
   return (
@@ -8,6 +9,36 @@ function Home() {
       <Hero />
       <AboutUs_1 />
       <Services_1 className="mt-14" />
+
+      <div className="mt-14 px-4 lg:px-10 w-full bg-black/95 py-36 grid grid-cols-4 gap-6 place-items-center">
+        <div className="relative">
+          <p className="text-white/90 text-3xl w-max font-medium absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 z-10">
+            Years of Experience
+          </p>
+          <AnimatedStatCount speed={100} start={1} end={19} counterClassName="!text-9xl !font-bold !text-slate-300/40" />
+        </div>
+
+        <div className="relative">
+          <p className="text-white/90 text-3xl w-max font-medium absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 z-10">
+           Projects Complete
+          </p>
+            <AnimatedStatCount speed={35} start={350} end={500} counterClassName="!text-9xl !font-bold !text-slate-300/40" />
+        </div>
+
+        <div className="relative">
+          <p className="text-white/90 text-3xl w-max font-medium absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 z-10">
+           Professional Team
+          </p>
+            <AnimatedStatCount speed={75} start={1} end={38} counterClassName="!text-9xl !font-bold !text-slate-300/40" />
+        </div>
+
+        <div className="relative">
+          <p className="text-white/90 text-3xl w-max font-medium absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 z-10">
+           Satisfied client
+          </p>
+            <AnimatedStatCount speed={35} start={420} end={689} counterClassName="!text-9xl !font-bold !text-slate-300/40" />
+        </div>
+      </div>
     </div>
   )
 }
