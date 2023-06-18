737964d3495bb965d7476e41d7b8dc29dabcdbb7
gitfname <newamailnam@gmail.com>
2023-06-18 13:58:03 +0430

    little change

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
-import App from './App.tsx'
-// const sleep = (ms:number) => {
-//   return new Promise(resovle => {
-//     setTimeout(() => {
-//       resovle(1)
-//     }, ms);
-//   })
-// }
-// const App = lazy(async () => {
-//   await sleep(8000)
-//   return import("./App.tsx")
-// })
+// import App from './App.tsx'
+const sleep = (ms:number) => {
+  return new Promise(resovle => {
+    setTimeout(() => {
+      resovle(1)
+    }, ms);
+  })
+}
+const App = lazy(async () => {
+  await sleep(8000)
+  return import("./App.tsx")
+})
 import './index.css'
 
 import './i18n.ts'
@@ -21,7 +21,7 @@ import Loading from './pages/Loading.tsx'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
       <BrowserRouter>
-        <Suspense>
+        <Suspense fallback={<Loading />}>
           <App />
         </Suspense>
       </BrowserRouter>
