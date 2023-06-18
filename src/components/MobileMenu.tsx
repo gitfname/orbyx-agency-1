a300381c2a9df00fbae02317fef2f202218dfee8
gitfname <newamailnam@gmail.com>
2023-06-18 13:56:56 +0430

    MobileMenu finished (i think so)

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,30 +1,39 @@
 import { useEffect, useState } from "react"
 import { AiOutlineMenu } from "react-icons/ai"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { useTranslation } from "react-i18next"
 import ChangeLangButton from "./ChangeLangButton"
 import { menuItems } from "../utils/http"
 import { menuItemOptions } from "../utils/http/menuItems"
+import {
+    Accordion,
+    AccordionButton,
+    AccordionIcon,
+    AccordionItem,
+    AccordionPanel,
+    Box
+} from "@chakra-ui/react"
 
 const getLang = (lng) => {
     const languages = ["en", "fa"]
-    return languages.find(v => v===lng) || "fa"
+    return languages.find(v => v === lng) || "fa"
 }
 
 function MobileMenu() {
     const [isOpen, setIsOpen] = useState(false)
     const [t, i18n] = useTranslation()
     const [items, setItems] = useState<menuItemOptions>()
+    const navigate = useNavigate()
+    
 
     useEffect(
         () => {
-            menuItems({lng: getLang(i18n.language)})
-            .then(data => setItems(data))
+            menuItems({ lng: getLang(i18n.language) })
+                .then(data => setItems(data))
         },
         [i18n.language]
     )
-    console.log(items);
-    
+
 
     return (
         <>
@@ -33,20 +42,20 @@ function MobileMenu() {
             </button>
             <div
                 className={`fixed top-0 left-0 w-full h-full pointer-events-none z-50
-                ${isOpen?"!pointer-events-auto":""}
+                ${isOpen ? "!pointer-events-auto" : ""}
                 `}
             >
                 <div
                     onClick={() => setIsOpen(false)}
                     className={`absolute top-0 left-0 z-10 w-full h-full bg-black/40 pointer-events-none opacity-0
                     transition-opacity duration-300
-                    ${isOpen?"!opacity-100 !pointer-events-auto":""}`}
+                    ${isOpen ? "!opacity-100 !pointer-events-auto" : ""}`}
                 ></div>
 
                 <div
                     className={`w-64 h-full bg-gray-900 absolute top-0 right-0 translate-x-full z-20
-                    flex flex-col gap-y-3 pt-6
-                    transition-transform duration-300 ${isOpen?"!translate-x-0":""}`}
+                    gap-y-3 pt-6 grid grid-rows-[max-content_1fr_max-content]
+                    transition-transform duration-300 ${isOpen ? "!translate-x-0" : ""}`}
                 >
 
                     <Link
@@ -55,8 +64,8 @@ function MobileMenu() {
                     >
                         {t("mobile-menu.title")}
                     </Link>
-                    
-                    <div dir={i18n.dir(i18n.language)} className="flex flex-col gap-y-2 px-3 mt-5">
+
+                    {/* <div dir={i18n.dir(i18n.language)} className="flex flex-col gap-y-2 px-3 mt-5">
                         {
                             items?.items?.map(item => (
                                 <Link
@@ -69,7 +78,70 @@ function MobileMenu() {
                                 </Link>
                             ))
                         }
-                    </div>
+                    </div> */}
+
+                    <Accordion
+                        allowMultiple
+                        display="flex"
+                        flexDirection="column"
+                        rowGap="5px"
+                        mx="4px"
+                        overflowY="auto"
+                    >
+
+                        {
+                            items?.items?.map(item => (
+                                <AccordionItem
+                                    key={item.id}
+                                    border="none"
+                                    color="whiteAlpha.800"
+                                >
+                                    <AccordionButton
+                                        onClick={() => item?.items?.length === 0 ? navigate(item.link) : null}
+                                        display="flex"
+                                        justifyContent="space-between"
+                                        transition="all"
+                                        transitionDuration="300ms"
+                                        rounded="lg"
+                                        _hover={{
+                                            bgColor: "whiteAlpha.100"
+                                        }}
+                                        _expanded={{
+                                            bgColor: item?.items?.length > 0 ? "whiteAlpha.100" : ""
+                                        }}
+                                    >
+                                        {item.text}
+                                        {
+                                            item?.items?.length > 0
+                                            ?
+                                                <AccordionIcon />
+                                            :
+                                                false
+                                        }
+                                    </AccordionButton>
+                                    <AccordionPanel padding={item?.items?.length > 0 ? "6px" : 0}>
+                                        {
+                                            item?.items?.map(item => (
+                                                <Box
+                                                    onClick={() => navigate(item.link)}
+                                                    key={item.id}
+                                                    borderLeft="1px"
+                                                    borderColor="whiteAlpha.200"
+                                                    px="4px"
+                                                >
+                                                    <p className="p-2 text-base font-[iranyekan300] px-3 rounded-lg hover:bg-white/5 transition-colors duration-300
+                                                    cursor-pointer text-white/90">{item.text}</p>
+                                                </Box>
+                                            ))
+                                        }
+                                    </AccordionPanel>
+                                </AccordionItem>
+                            ))
+                        }
+
+
+                    </Accordion>
+
 
                     <div className="mt-auto flex items-center gap-x-2 justify-center mb-6 mx-auto w-max rounded-xl
                     border border-slate-700">
--- src/utils/http/menuItems/index.ts
+++ src/utils/http/menuItems/index.ts
@@ -3,7 +3,12 @@ import { ApplicationRoutes } from "../../../routes"
 interface menuItemsInnerItemsOptions {
     id: string,
     text: string,
-    link: string
+    link: string,
+    items: Array<{
+        id: string,
+        text: string,
+        link: string,
+    }>
 }
 
 export interface menuItemOptions {
@@ -17,8 +22,8 @@ interface menuItemsProps {
 }
 
 async function menuItems({ lng }: menuItemsProps): Promise<menuItemOptions> {
-    return new Promise((resolve):menuItemOptions => {
-        if(lng === "en") {
+    return new Promise((resolve): menuItemOptions => {
+        if (lng === "en") {
             resolve(
                 {
                     account_text: "",
@@ -27,27 +32,90 @@ async function menuItems({ lng }: menuItemsProps): Promise<menuItemOptions> {
                         {
                             "id": "0",
                             "text": "Home",
-                            "link": ApplicationRoutes.pages.home
+                            "link": ApplicationRoutes.pages.home,
+                            items: []
                         },
                         {
                             "id": "1",
                             "text": "Company",
-                            "link": import.meta.env.BASE_URL+"/still-in-dev"
+                            "link": import.meta.env.BASE_URL + "/still-in-dev",
+                            items: [
+                                {
+                                    id: "0",
+                                    text: "About Us",
+                                    link: ApplicationRoutes.pages.company["about-us"]
+                                },
+                                {
+                                    id: "1",
+                                    text: "Our Team",
+                                    link: ApplicationRoutes.pages.company["our-team"]
+                                },
+                                {
+                                    id: "2",
+                                    text: "Services",
+                                    link: ApplicationRoutes.pages.company["services"]
+                                }
+                            ]
                         },
                         {
                             "id": "2",
                             "text": "Work",
-                            "link": import.meta.env.BASE_URL+"/still-in-dev"
+                            "link": "",
+                            items: [
+                                {
+                                    id: "0",
+                                    text: "Projects",
+                                    link: ApplicationRoutes.pages.work.project
+                                },
+                                {
+                                    id: "1",
+                                    text: "Project Details",
+                                    link: ApplicationRoutes.pages.work["project-details"]
+                                },
+                                {
+                                    id: "1",
+                                    text: "Best Projects",
+                                    link: ApplicationRoutes.pages.work["best-projects"]
+                                }
+                            ]
                         },
                         {
                             "id": "2",
                             "text": "Blog",
-                            "link": import.meta.env.BASE_URL+"/still-in-dev"
+                            "link": "",
+                            items: [
+                                {
+                                    id: "0",
+                                    text: "Blog",
+                                    link: ApplicationRoutes.pages.blog.blog
+                                },
+                                {
+                                    id: "1",
+                                    text: "Latest Articles",
+                                    link: ApplicationRoutes.pages.blog["latest-articles"]
+                                },
+                                {
+                                    id: "2",
+                                    text: "Recommended for You",
+                                    link: ApplicationRoutes.pages.blog["recommended-for-you"]
+                                },
+                                {
+                                    id: "3",
+                                    text: "Today's top Articles",
+                                    link: ApplicationRoutes.pages.blog["best-of-day"]
+                                },
+                                {
+                                    id: "4",
+                                    text: "This Week top Articles",
+                                    link: ApplicationRoutes.pages.blog["best-of-week"]
+                                }
+                            ]
                         },
                         {
                             "id": "2",
                             "text": "Contact Us",
-                            "link": ApplicationRoutes.pages.contact
+                            "link": ApplicationRoutes.pages.contact,
+                            items: []
                         }
                     ]
                 }
@@ -56,33 +124,96 @@ async function menuItems({ lng }: menuItemsProps): Promise<menuItemOptions> {
         else if (lng === "fa") {
             resolve(
                 {
-                    "title": "Orbyx",
-                    "account_text": "حساب کاربری",
-                    "items": [
+                    account_text: "",
+                    title: "",
+                    items: [
                         {
                             "id": "0",
                             "text": "خانه",
-                            "link": ApplicationRoutes.pages.home
+                            "link": ApplicationRoutes.pages.home,
+                            items: []
                         },
                         {
                             "id": "1",
                             "text": "شرکت",
-                            "link": import.meta.env.BASE_URL+"/still-in-dev"
+                            "link": "",
+                            items: [
+                                {
+                                    id: "0",
+                                    text: "درباره ما",
+                                    link: ApplicationRoutes.pages.company["about-us"]
+                                },
+                                {
+                                    id: "1",
+                                    text: "تیم ما",
+                                    link: ApplicationRoutes.pages.company["our-team"]
+                                },
+                                {
+                                    id: "2",
+                                    text: "خدمات ما",
+                                    link: ApplicationRoutes.pages.company["services"]
+                                }
+                            ]
                         },
                         {
                             "id": "2",
                             "text": "کار",
-                            "link": import.meta.env.BASE_URL+"/still-in-dev"
+                            "link": "",
+                            items: [
+                                {
+                                    id: "0",
+                                    text: "پروژه ها",
+                                    link: ApplicationRoutes.pages.work.project
+                                },
+                                {
+                                    id: "1",
+                                    text: "جزیات پروژه",
+                                    link: ApplicationRoutes.pages.work["project-details"]
+                                },
+                                {
+                                    id: "1",
+                                    text: "پروژه های تاپ",
+                                    link: ApplicationRoutes.pages.work["best-projects"]
+                                }
+                            ]
                         },
                         {
                             "id": "2",
                             "text": "وبلاگ",
-                            "link": ApplicationRoutes.pages.blog.blog
+                            "link": "",
+                            items: [
+                                {
+                                    id: "0",
+                                    text: "وبلاگ",
+                                    link: ApplicationRoutes.pages.blog.blog
+                                },
+                                {
+                                    id: "1",
+                                    text: "آخرین مقالات",
+                                    link: ApplicationRoutes.pages.blog["latest-articles"]
+                                },
+                                {
+                                    id: "2",
+                                    text: "پیشنهادی برای شما",
+                                    link: ApplicationRoutes.pages.blog["recommended-for-you"]
+                                },
+                                {
+                                    id: "3",
+                                    text: "برترین های امروز",
+                                    link: ApplicationRoutes.pages.blog["best-of-day"]
+                                },
+                                {
+                                    id: "4",
+                                    text: "برترین های این هفته",
+                                    link: ApplicationRoutes.pages.blog["best-of-week"]
+                                }
+                            ]
                         },
                         {
                             "id": "2",
-                            "text": "ارتباط  با ما",
-                            "link": ApplicationRoutes.pages.contact
+                            "text": "ارتباط با ما",
+                            "link": ApplicationRoutes.pages.contact,
+                            items: []
                         }
                     ]
                 }
